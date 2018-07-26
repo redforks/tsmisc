7c87862d1dca9f30f98292f414b5bf5a9946bc64
Red Forks <redforks@gmail.com>
2018-07-26 14:27:54 +0800

    Add guid()

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
 export declare function delay<T>(ms: number, v?: T): Promise<T>;
+export declare function guid(): string;
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@
 export function delay<T>(ms: number, v?: T): Promise<T> {
   return new Promise((resolve) => setTimeout(() => resolve(v), ms));
 }
+
+/**
+ * Returns a random unique guid string.
+ */
+export function guid(): string {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
